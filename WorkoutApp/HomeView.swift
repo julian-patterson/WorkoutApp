9b20fbd5fba91b1ffb38ba437941dbd38a2d2116
julian-patterson <julianedwardpatterson@gmail.com>
2024-02-20 23:38:53 -0500

    Adding a Calendar!!

--- WorkoutApp/HomeView.swift
+++ WorkoutApp/HomeView.swift
@@ -7,14 +7,28 @@
 
 import SwiftUI
 
+
 struct HomeView: View {
+    @State var selectedDate: Date = Date()
     var body: some View {
         List {
-            Section(header: headerView()) {
-                Text("Content")
+            VStack() {
+                Text("Today:\n"  + selectedDate.formatted(date: .abbreviated, time: .omitted))
+                    .font(.system(size: 32))
+                    .bold()
+                    .foregroundColor(Color.orange)
+                    .padding()
+                    .animation(.spring(), value: selectedDate)
+                    .frame(width: 400, alignment: .topLeading)
+//                Divider().frame(height: 1)
+                DatePicker("Select Date", selection: $selectedDate, displayedComponents: [.date])
+                    .padding(.horizontal)
+                    .datePickerStyle(.graphical)
+//                Divider()
             }
+            .padding(.vertical, 10)
         }
-        .listStyle(GroupedListStyle())
+        .listStyle(.plain)
         .navigationBarTitle("Home", displayMode: .large)
         .navigationBarHidden(false)
     }
